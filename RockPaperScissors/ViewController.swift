e23b19eb5a6d982169f8e5c84757ffd89bc1743a
angelicaalemanc <67842987+angelicaalemanc@users.noreply.github.com>
2022-08-19 19:53:27 -0600

    Methods for buttons (on click)

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -14,6 +14,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    // button action for the image of rock
+    @IBAction func clickRock(_ sender: UIButton) {
+        print("rock")
+    }
+    
+    // button action for the image of paper
+    @IBAction func clickPaper(_ sender: UIButton) {
+        print("paper")
+    }
+    
+    // button action for the image of scissors
+    @IBAction func clickScissors(_ sender: UIButton) {
+        print("scissors")
+    }
+    
+    // button action for restart
+    @IBAction func clickRestart(_ sender: UIButton) {
+        print("restart")
+    }
 }
 
